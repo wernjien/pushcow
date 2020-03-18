fe67fde790b05f594df124fbfec006013fd6a388
Tan Wern Jien <wernjien@gmail.com>
2020-03-18 22:00:11 +0800

    Models and migrations for application and device

---
+++ app/Application.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Application extends Model implements AuthenticatableContract
+{
+    use Authenticatable, SoftDeletes;
+}
