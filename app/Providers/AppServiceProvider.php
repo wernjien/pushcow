e7b471586ae56329ba110cfb0393cc084bb6ec74
Tan Wern Jien <wernjien@gmail.com>
2020-03-27 11:36:11 +0800

    Set default string length to 191
    For MySQL <= v5.7.7 or MariaDB <= v10.2.2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Device;
 use App\Observers\DeviceObserver;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         Device::observe(DeviceObserver::class);
     }
 }
