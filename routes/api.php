e75013e5122d275ce4e22877f228538d2c5ab5b4
Tan Wern Jien <wernjien@gmail.com>
2020-04-01 13:45:56 +0800

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -12,5 +12,6 @@
 */
 
 Route::prefix('v1')->group(function () {
-    Route::resource('devices', 'DeviceController')->only(['store', 'destroy']);
+    Route::post('devices', 'DeviceController@store');
+    Route::delete('devices/{device}', 'DeviceController@destroy');
 });
