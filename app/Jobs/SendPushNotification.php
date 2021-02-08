146b42e72ec808e1ab046d6f85ecbc236db4a260
Tan Wern Jien <wernjien@gmail.com>
2021-02-08 10:20:33 +0800

    Send push notification job

---
+++ app/Jobs/SendPushNotification.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Services\PushNotification as PushCow;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendPushNotification implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        (new PushCow)->push();
+    }
+}
