970f108d349981ebd8ceadda627451946266fc9e
Tan Wern Jien <wernjien@gmail.com>
2022-06-16 15:42:16 +0800

    Leverage create message to job

---
+++ app/Jobs/CreateMessage.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Message;
+use App\MessageRequest;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class CreateMessage implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * The message request instance.
+     *
+     * @var \App\MessageRequest
+     */
+    protected $request;
+
+    /**
+     * The recipient device ID.
+     *
+     * @var string
+     */
+    protected $deviceId;
+
+    /**
+     * The recipient user's ID.
+     *
+     * @var string
+     */
+    protected $userId;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(MessageRequest $request, $deviceId, $userId)
+    {
+        $this->request = $request;
+        $this->deviceId = $deviceId;
+        $this->userId = $userId;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $request = $this->request;
+        $deviceId = $this->deviceId;
+        $data = $this->getData();
+        $now = now();
+
+        Message::insert([
+            'device_id' => $deviceId,
+            'notification' => $request->notification,
+            'data' => $data,
+            'options' => $request->options,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+    }
+
+    /**
+     * Process and return the message data.
+     *
+     * @return string
+     */
+    protected function getData()
+    {
+        $data = $this->request->data;
+        $userId = $this->userId;
+
+        if (empty($userId)) {
+            return $data;
+        }
+
+        $data = array_merge(['_user_id' => $userId], json_decode($data, true));
+
+        return json_encode($data);
+    }
+}
--- app/Jobs/ProcessMessageRequests.php
+++ app/Jobs/ProcessMessageRequests.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use DB;
 use App\Message;
 use App\MessageRequest;
+use App\Jobs\CreateMessage;
 use App\Jobs\ForwardMessages;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -47,46 +48,16 @@ class ProcessMessageRequests implements ShouldQueue
             $devices = $application->devices()->search($request->recipients);
 
             $devices->chunk(250, function ($devices) use ($request) {
-                $now = now();
-                $data = [];
+                $data = $devices->pluck('user_id', 'id');
 
-                foreach ($devices as $device) {
-                    $data[] = [
-                        'device_id' => $device->id,
-                        'notification' => $request->notification,
-                        'data' => $this->prependUserId($request->data, $device),
-                        'options' => $request->options,
-                        'created_at' => $now,
-                        'updated_at' => $now,
-                    ];
+                foreach ($data as $deviceId => $userId) {
+                    CreateMessage::dispatch($request, $deviceId, $userId);
                 }
-
-                Message::insert($data);
             });
 
             $request->delete();
         });
 
         ForwardMessages::dispatch();
-    }
-
-    /**
-     * Prepend user ID to data.
-     *
-     * @param  string  $data
-     * @param  \App\Device  $device
-     * @return string
-     */
-    protected function prependUserId($data, $device)
-    {
-        $userId = data_get($device, 'user_id');
-
-        if (empty($userId)) {
-            return $data;
-        }
-
-        $data = array_merge(['_user_id' => $userId], json_decode($data, true));
-
-        return json_encode($data);
     }
 }
