9adf1ac52447d70d248f495976cd942672278508
Tan Wern Jien <wernjien@gmail.com>
2024-03-21 07:46:31 +0800

    Remove unnecessary declaration

--- app/Jobs/ProcessMessageRequests.php
+++ app/Jobs/ProcessMessageRequests.php
@@ -4,7 +4,6 @@ namespace App\Jobs;
 
 use App\Message;
 use App\MessageRequest;
-use DB;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
