37a5824520d73810afb7c2015befe5d2da2c723c
Tan Wern Jien <wernjien@gmail.com>
2024-03-26 13:03:25 +0800

    Make platform as optional field in v3 register device

--- app/Http/Requests/V3/RegisterDevice.php
+++ app/Http/Requests/V3/RegisterDevice.php
@@ -2,6 +2,26 @@
 
 namespace App\Http\Requests\V3;
 
+use App\Device;
 use App\Http\Requests\V2\RegisterDevice as RegisterDeviceV2;
+use Illuminate\Validation\Rule;
 
-class RegisterDevice extends RegisterDeviceV2 {}
+class RegisterDevice extends RegisterDeviceV2
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $platforms = Device::getPlatforms();
+
+        return [
+            'platform' => ['nullable', 'string', Rule::in($platforms)],
+            'device_id' => ['required', 'string'],
+            'token' => ['required', 'string'],
+            'user_id' => ['nullable', 'string'],
+        ];
+    }
+}
