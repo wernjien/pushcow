8d63227fae9a95469d2fa8d58109ab105be34585
Tan Wern Jien <wernjien@gmail.com>
2024-03-26 13:21:13 +0800

    Fix versioning controllers logic

--- app/Http/Controllers/V2/DeviceController.php
+++ app/Http/Controllers/V2/DeviceController.php
@@ -2,6 +2,46 @@
 
 namespace App\Http\Controllers\V2;
 
-use App\Http\Controllers\V1\DeviceController as DeviceControllerV1;
+use App\Device;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V2\DeleteDevice;
+use App\Http\Requests\V2\RegisterDevice;
+use App\Http\Resources\Device as DeviceResource;
+use App\Repositories\DeviceRepository;
+use App\Support\Response;
 
-class DeviceController extends DeviceControllerV1 {}
+class DeviceController extends Controller
+{
+    /**
+     * Register or update an existing device.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(RegisterDevice $request)
+    {
+        $data = $request->validated();
+        $deviceId = $request->input('device_id');
+
+        $device = DeviceRepository::find($deviceId);
+
+        if (! $device instanceof Device) {
+            $device = DeviceRepository::create($data);
+        } else {
+            DeviceRepository::update($device, $data);
+        }
+
+        return Response::success(new DeviceResource($device));
+    }
+
+    /**
+     * Unregister existing devices.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DeleteDevice $request)
+    {
+        DeviceRepository::delete($request->validated());
+
+        return Response::success();
+    }
+}
--- app/Http/Controllers/V3/MessageController.php
+++ app/Http/Controllers/V3/MessageController.php
@@ -2,6 +2,31 @@
 
 namespace App\Http\Controllers\V3;
 
-use App\Http\Controllers\V2\MessageController as MessageControllerV2;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V3\CreateMessageRequest;
+use App\Jobs\ProcessMessageRequests;
+use App\Repositories\MessageRequestRepository;
+use App\Support\Response;
 
-class MessageController extends MessageControllerV2 {}
+class MessageController extends Controller
+{
+    /**
+     * Receive a create message request.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateMessageRequest $request)
+    {
+        $recipients = $request->input('recipients');
+        $notification = $request->input('notification');
+        $data = $request->input('data');
+        $options = $request->input('options');
+        $data = compact('recipients', 'notification', 'data', 'options');
+
+        ProcessMessageRequests::dispatch(
+            MessageRequestRepository::create($data)
+        );
+
+        return Response::success();
+    }
+}
