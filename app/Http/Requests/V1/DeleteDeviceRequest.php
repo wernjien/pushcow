a7e6eb4dd0144dd0b14566f5bb9f097c8a127c4f
Tan Wern Jien <wernjien@gmail.com>
2024-03-27 15:44:40 +0800

    Fix request class name

--- app/Http/Requests/V1/DeleteDeviceRequest.php
+++ app/Http/Requests/V1/DeleteDeviceRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class DeleteDevice extends FormRequest
+class DeleteDeviceRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
