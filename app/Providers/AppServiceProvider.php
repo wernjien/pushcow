017d00f27afa99a35bfa462c73cbe094b93aac8e
Tan Wern Jien <wernjien@gmail.com>
2024-05-27 14:11:59 +0800

    Install Telescope

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment(['local', 'dev'])) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
