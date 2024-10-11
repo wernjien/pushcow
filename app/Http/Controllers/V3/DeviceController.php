56c2967017a539f65eabb66c103e23a17c45c7d1
Tan Wern Jien <wernjien@gmail.com>
2024-10-11 11:00:01 +0800

    Queued register device

--- app/Http/Controllers/V3/DeviceController.php
+++ app/Http/Controllers/V3/DeviceController.php
@@ -3,28 +3,32 @@
 namespace App\Http\Controllers\V3;
 
 use App\Actions\DeleteDevice;
-use App\Actions\RegisterDevice;
+use App\Actions\QueuedRegisterDevice;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V3\DeleteDeviceRequest;
 use App\Http\Requests\V3\RegisterDeviceRequest;
-use App\Http\Resources\Device as DeviceResource;
 use App\Support\Response;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Arr;
 
 class DeviceController extends Controller
 {
     /**
      * Register or update an existing device.
      */
-    public function store(RegisterDeviceRequest $request, RegisterDevice $register): JsonResponse
+    public function store(RegisterDeviceRequest $request, QueuedRegisterDevice $register): JsonResponse
     {
         $data = $request->validated();
         $deviceId = $request->input('device_id');
         $attribute = compact('deviceId');
 
-        $device = tap($register->execute($attribute, $data))->subscribe('global');
+        if (! Arr::has($data, 'application_id') && auth()->check()) {
+            $data['application_id'] = auth()->id();
+        }
 
-        return Response::success(new DeviceResource($device));
+        $register->onQueue()->execute($attribute, $data, 'global');
+
+        return Response::success();
     }
 
     /**
