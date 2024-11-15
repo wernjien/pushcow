bd443c91f9b45f9b881c321f4bc212dcaf3a63ec
Tan Wern Jien <wernjien@gmail.com>
2024-11-15 14:38:47 +0800

    Revert queueable device registration to non-queueable

--- app/Http/Controllers/V3/DeviceController.php
+++ app/Http/Controllers/V3/DeviceController.php
@@ -3,32 +3,28 @@
 namespace App\Http\Controllers\V3;
 
 use App\Actions\DeleteDevice;
-use App\Actions\QueuedRegisterDevice;
+use App\Actions\RegisterDevice;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V3\DeleteDeviceRequest;
 use App\Http\Requests\V3\RegisterDeviceRequest;
+use App\Http\Resources\Device as DeviceResource;
 use App\Support\Response;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Arr;
 
 class DeviceController extends Controller
 {
     /**
      * Register or update an existing device.
      */
-    public function store(RegisterDeviceRequest $request, QueuedRegisterDevice $register): JsonResponse
+    public function store(RegisterDeviceRequest $request, RegisterDevice $register): JsonResponse
     {
         $data = $request->validated();
         $deviceId = $request->input('device_id');
         $attribute = compact('deviceId');
 
-        if (! Arr::has($data, 'application_id') && auth()->check()) {
-            $data['application_id'] = auth()->id();
-        }
+        $device = tap($register->execute($attribute, $data))->subscribe('global');
 
-        $register->onQueue()->execute($attribute, $data, 'global');
-
-        return Response::success();
+        return Response::success(new DeviceResource($device));
     }
 
     /**
