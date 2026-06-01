8b664e1a1de109204390368fe535ea3ae943b6e6
Tan Wern Jien <wernjien@gmail.com>
2026-06-01 17:42:47 +0800

    Remove legacy FCM

--- routes/api.php
+++ routes/api.php
@@ -11,29 +11,9 @@
 |
 */
 
-use App\Http\Controllers\V1\DeviceController as DeviceControllerV1;
-use App\Http\Controllers\V1\MessageController as MessageControllerV1;
-use App\Http\Controllers\V1\Pulse as PulseV1;
-use App\Http\Controllers\V2\DeviceController as DeviceControllerV2;
-use App\Http\Controllers\V2\MessageController as MessageControllerV2;
-use App\Http\Controllers\V2\Pulse as PulseV2;
 use App\Http\Controllers\V3\DeviceController as DeviceControllerV3;
 use App\Http\Controllers\V3\MessageController as MessageControllerV3;
 use App\Http\Controllers\V3\Pulse as PulseV3;
-
-Route::prefix('v1')->group(function () {
-    Route::get('/', PulseV1::class);
-    Route::post('devices', [DeviceControllerV1::class, 'store']);
-    Route::delete('devices', [DeviceControllerV1::class, 'destroy']);
-    Route::post('messages', [MessageControllerV1::class, 'store']);
-});
-
-Route::prefix('v2')->group(function () {
-    Route::get('/', PulseV2::class);
-    Route::post('devices', [DeviceControllerV2::class, 'store']);
-    Route::delete('devices', [DeviceControllerV2::class, 'destroy']);
-    Route::post('messages', [MessageControllerV2::class, 'store']);
-});
 
 Route::prefix('v3')->group(function () {
     Route::get('/', PulseV3::class);
